48ebfa17997c73296f5514208a2a898b873851f8
aki-senri <ch_chulainn_curse@hotmail.com>
2023-11-24 05:21:13 +0900

    プロジェクト ファイルを追加します。

---
+++ CommonControl/Controls/HasDefaultComboBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommonControl.Controls
+{
+    /// <summary>
+    /// HasDefaultComboBox.xaml の相互作用ロジック
+    /// </summary>
+    public partial class HasDefaultComboBox : UserControl
+    {
+        public HasDefaultComboBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty DefalutIndexProperty =
+            DependencyProperty.Register("DefalutIndex", typeof(int), typeof(HasDefaultComboBox));
+
+        public int DefalutIndex
+        {
+            get { return (int)GetValue(DefalutIndexProperty); }
+            set { SetValue(DefalutIndexProperty, value); }
+        }
+
+        public bool IsDefaultIndex
+        {
+            get { return SelectedIndex == DefalutIndex; }
+        }
+    }
+}
